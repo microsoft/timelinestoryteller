172b307582ac48364066980962df6e9b77b3b423
Matt Brehmer <mabrehme@microsoft.com>
2017-01-20 15:08:58 -0800

    ssl disabling

--- DisableSslv3.cmd
+++ DisableSslv3.cmd
@@ -1,5 +1,5 @@
 echo This is DisableSslv3.cmd
 
-PowerShell -ExecutionPolicy Unrestricted "%DEPLOYMENT_SOURCE%\DisableSslv3.ps1" >> "%DEPLOYMENT_TEMP%\Startuplog.txt" 2>&1
+PowerShell -ExecutionPolicy Unrestricted .\DisableSslv3.ps1 >> "%DEPLOYMENT_TEMP%\Startuplog.txt" 2>&1
 
 EXIT /B 0
