ebeb11c38b8b8761fa60e63f830f439a963737dd
David Tittsworth <stopyoukid@gmail.com>
2017-05-11 15:16:27 -0700

    Added a fix for the broken legend, Fixes #33

---
+++ src/utils.spec.js
@@ -0,0 +1,17 @@
+var d3 = require("d3");
+var utils = require("./utils");
+var expect = require("chai").expect;
+
+describe("utils", function () {
+  describe("setScaleValue", function() {
+    it("should replace a scale value correctly", function () {
+        var scale = d3.scale.ordinal();
+        scale.range([1, 2, 3]);
+        scale.domain(["B", "A", "C"]);
+
+        utils.setScaleValue(scale, "A", 200);
+
+        expect(scale("A")).to.be.equal(200);
+    });
+  });
+});
